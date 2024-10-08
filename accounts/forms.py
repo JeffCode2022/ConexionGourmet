c036cc19649ce376655c65554b051408744de910
JeffCode2022 <109402303+JeffCode2022@users.noreply.github.com>
2024-10-08 00:34:28 -0500

    hasta el vprofile de vendor

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import User
+from .models import User, UserProfile
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
@@ -23,3 +23,11 @@ class UserForm(forms.ModelForm):
         if commit:
             user.save()
         return user
+    
+class UserProfileForm(forms.ModelForm):
+    profile_picture = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+    cover_photo = forms.ImageField(widget=forms.FileInput(attrs={'class':'btn btn-info'}))
+    
+    class Meta:
+        model = UserProfile
+        fields = ['profile_picture', 'cover_photo', 'address_line_1','address_line_2', 'city', 'state', 'zip_code', 'country','latitude', 'longitude']
